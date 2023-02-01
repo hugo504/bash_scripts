c09a41199ddbbe732c487aa471f8054632332541
Hugo Bacard <hbacard@pm.me>
2023-02-01 15:34:22 +0100

    update search range in pinger.sh

--- pinger.sh
+++ pinger.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 online="online_host.txt"
 offline="offline_host.txt"
-for i in XX.XX.XXX.{196..255} # Replace XX.XX.XXX by network address
+for i in XX.XX.XXX.{1..255} # Replace XX.XX.XXX by network address
 do
 ping -c 1 $i
 if [ $? -eq 0 ]; then
