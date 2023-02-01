d74d089733d61b5523e1e4371ed3d429afe65017
Hugo Bacard <hbacard@pm.me>
2023-02-01 15:32:11 +0100

    updates pinger: add offline_host file

--- pinger.sh
+++ pinger.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 online="online_host.txt"
-
-for i in XX.XXX.XX.{196..255} # Replace by base ip address
+offline="offline_host.txt"
+for i in XX.XX.XXX.{196..255} # Replace XX.XX.XXX by network address
 do
 ping -c 1 $i
 if [ $? -eq 0 ]; then
 echo $i >> $online
+else
+echo $i >> $offline
 fi
 done
-
